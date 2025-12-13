96733210bd5e76de433939be9241e0d92af6af6d
Morgan Hunter <32530812+mxc42@users.noreply.github.com>
2025-12-12 21:49:18 -0500

    Fix parsing of last element

--- src/model/resultModel.ts
+++ src/model/resultModel.ts
@@ -21,7 +21,7 @@ export class ResultModel {
             buffer.subarray(++i, i = buffer.indexOf(44, i)).toString('ascii'),
             buffer.subarray(++i, i = buffer.indexOf(44, i)).toString('ascii'),
             buffer.subarray(++i, i = buffer.indexOf(44, i)).toString('ascii').trimEnd(),
-            buffer.subarray(++i, i = buffer.indexOf(59, i) + 1).toString('ascii'),
+            buffer.subarray(++i).toString('ascii'),
         );
     }
 }
